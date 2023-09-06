12b9c848c5617d28662c41d70f7cbaa43f7e5e37
ali <ali@gmail.com>
2023-09-06 19:10:13 +0500

    minor change

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ const swaggerUI=require('swagger-ui-express')
 const YAML=require('yamljs')
 const swaggerDocument=YAML.load('./swagger.yaml')
 
-
-const express = require('express');
-const app = express();
-
-app.use(express.json());
 //db connection
 const connectDB = require('./db/connect');
 
@@ -28,21 +23,10 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const auth = require('./middleware/authentication');
 
-// extra packages
+const express = require('express');
+const app = express();
 
-// routes
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs APIS</h1><a href="/api-docs">Documentation</a>');
-});
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument))
-app.use('/api/v1/auth',authRoute)
-app.use('/api/v1/jobs',auth,jobsRoute)
-    
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
-
-//security
-app.set('trust proxy', 1);
+app.use(express.json());
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
@@ -52,6 +36,21 @@ app.use(
 app.use(helmet())
 app.use(cors({ origin: '*' }));
 app.use(xss)
+
+// extra packages
+
+// routes
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs APIS</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/api/v1/auth',authRoute)
+app.use('/api/v1/jobs',auth,jobsRoute)
+    
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+//security
 
 const port = process.env.PORT || 3000;
 
