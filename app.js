84b92d440e07457a1d2ec510d8ba146199e5eacf
ali <ali@gmail.com>
2023-09-06 19:02:35 +0500

    swagger docs added

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const cors=require('cors')
 const xss=require('xss-clean')
 const rateLimiter=require('express-rate-limit')
 
+//swagger
+const swaggerUI=require('swagger-ui-express')
+const YAML=require('yamljs')
+const swaggerDocument=YAML.load('./swagger.yaml')
+
+
 const express = require('express');
 const app = express();
 
@@ -28,6 +34,7 @@ const auth = require('./middleware/authentication');
 app.get('/', (req, res) => {
   res.send('<h1>Jobs APIS</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument))
 app.use('/api/v1/auth',authRoute)
 app.use('/api/v1/jobs',auth,jobsRoute)
     
